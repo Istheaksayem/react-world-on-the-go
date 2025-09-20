687ddcee85d4a713eabaed48ed2b827fef6b7e7f
Istheaksayem <istheakahmed9345@gmail.com>
2025-09-20 12:06:30 +0600

    added countries git coloum

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
 
+import { Suspense } from 'react'
 import './App.css'
 import Countries from './compunents/countries/countries'
+const countriesPromies = fetch('https://openapi.programming-hero.com/api/all')
+  .then(res => res.json())
 
 function App() {
 
 
   return (
     <>
-      
-<Countries></Countries>
-     
-      
+
+    <Suspense fallback={<p>Nadir vai loding..</p>}>
+        <Countries countriesPromies={countriesPromies}></Countries>
+    </Suspense>
+
+
     </>
   )
 }
--- src/compunents/countries/countries.jsx
+++ src/compunents/countries/countries.jsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { use } from 'react';
+import Country from './Country/country';
+import './countries.css'
 
-const Countries = () => {
+const Countries = ({countriesPromies}) => {
+
+    const CountriesData = use(countriesPromies);
+    const countries =CountriesData.countries;
+ 
     return (
         <div>
-            <h1>In the countries</h1>
+            <h1>In the countries: {countries.length}</h1>
+      <div id='countries'>
+          {
+            countries.map(country => <Country key={country.cca3.cca3} country={country}></Country>)
+        }
+      </div>
         </div>
     );
 };
